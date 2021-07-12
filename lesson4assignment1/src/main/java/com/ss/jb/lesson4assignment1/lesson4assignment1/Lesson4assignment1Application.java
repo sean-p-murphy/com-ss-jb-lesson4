a85d2cb1569977e15c49af1163e91c1a5850109f
murphcoder <murphcoder@gmail.com>
2021-07-12 16:46:14 -0500

    assignment 1 completed

--- lesson4assignment1/src/main/java/com/ss/jb/lesson4assignment1/lesson4assignment1/Lesson4assignment1Application.java
+++ lesson4assignment1/src/main/java/com/ss/jb/lesson4assignment1/lesson4assignment1/Lesson4assignment1Application.java
@@ -1,13 +1,37 @@
 package com.ss.jb.lesson4assignment1.lesson4assignment1;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class Lesson4assignment1Application {
 
+	public static class Singleton {
+
+		public String test;
+
+		private static Singleton instance;
+  
+		private Singleton() {}
+  
+		public static Singleton getInstance() {
+
+			if (instance == null) {
+				synchronized (Singleton.class) {
+					if (instance == null) {
+						instance = new Singleton();
+					}
+				}
+        	}
+	        return instance;
+		}
+    }
+
 	public static void main(String[] args) {
-		SpringApplication.run(Lesson4assignment1Application.class, args);
+		Singleton first = Singleton.getInstance();
+		Singleton second = Singleton.getInstance();
+
+		first.test = "Pickle";
+		second.test = "Bagel";
+
+		System.out.println(first.test);
+		System.out.println(second.test);
 	}
 
 }
