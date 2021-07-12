8e06258db80817dada7601a5256e5bb26e918659
murphcoder <murphcoder@gmail.com>
2021-07-12 13:31:28 -0500

    first commit

---
+++ lesson4assignment1/src/main/java/com/ss/jb/lesson4assignment1/lesson4assignment1/Lesson4assignment1Application.java
@@ -0,0 +1,13 @@
+package com.ss.jb.lesson4assignment1.lesson4assignment1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson4assignment1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lesson4assignment1Application.class, args);
+	}
+
+}
---
+++ lesson4assignment1/src/test/java/com/ss/jb/lesson4assignment1/lesson4assignment1/Lesson4assignment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ss.jb.lesson4assignment1.lesson4assignment1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson4assignment1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
